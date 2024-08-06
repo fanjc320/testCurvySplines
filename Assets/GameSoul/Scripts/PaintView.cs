5c69126645a62c7aeda6638f51a999a54d4da63f
jackyfan <894020798@qq.com>
2024-08-06 17:18:55 +0800

    整理ok!

--- Assets/GameSoul/Scripts/PaintView.cs
+++ Assets/GameSoul/Scripts/PaintView.cs
@@ -133,8 +133,6 @@ public class PaintView : MonoBehaviour
             _brushSizeText=slider.transform.Find("Background/Text").GetComponent<Text>();
         }
         _brushSizeText.text = slider.value.ToString("f2");
-
-        TestMatrix();
     }
 
     /// <summary>
@@ -161,8 +159,6 @@ public class PaintView : MonoBehaviour
     }
 
     #endregion
-
-    #region 内部函数
 	
     //初始化数据
     void InitData()
@@ -186,7 +182,6 @@ public class PaintView : MonoBehaviour
             _screenHeight = Screen.height;
 
             _renderTex = RenderTexture.GetTemporary(_screenWidth, _screenHeight, 24);
-            //_paintCanvas.texture = _renderTex;
             _canvasTex = RenderTexture.GetTemporary(_screenWidth, _screenHeight, 24);
             _paintCanvas.texture = _canvasTex;
         }
@@ -201,13 +196,13 @@ public class PaintView : MonoBehaviour
         _paintBrushMat = new Material(_paintBrushShader);
         _paintBrushMat.SetTexture("_BrushTex", _defaultBrushTex);
         _paintBrushMat.SetColor("_Color", _defaultColor);
-        _paintBrushMat.SetFloat("_Size", _brushSize);
+        //_paintBrushMat.SetFloat("_Size", _brushSize);
     }
 
     //插点
     private void LerpPaint(Vector2 point)
     {
-        Paint(point);
+        //Paint(point);
 
         if (_lastPoint == Vector2.zero)
         {
@@ -215,19 +210,19 @@ public class PaintView : MonoBehaviour
             return;
         }
 
-        float brushWid = _brushSize / _screenWidth;
-        float brushHei = _brushSize / _screenHeight;
-        _paintBrushMat.SetFloat("_BrushUVWidth", brushWid);
-        _paintBrushMat.SetFloat("_BrushUVHeight", brushHei);
+        //float brushWid = _brushSize / _screenWidth;
+        //float brushHei = _brushSize / _screenHeight;
+        //_paintBrushMat.SetFloat("_BrushUVWidth", brushWid);
+        //_paintBrushMat.SetFloat("_BrushUVHeight", brushHei);
 
         float angleOfLine = Mathf.Atan2((point.y - _lastPoint.y), (point.x - _lastPoint.x)) * 180 / Mathf.PI;
-        Quaternion rot = Quaternion.Euler(0, 0, angleOfLine);
+        //Quaternion rot = Quaternion.Euler(0, 0, angleOfLine);
         Quaternion rotReverse = Quaternion.Euler(0, 0, -angleOfLine);
 
         Vector2 pointUV = new Vector2(point.x/_screenWidth, point.y/_screenHeight);
         Matrix4x4 matScaleRecover = Matrix4x4.Scale(new Vector3(_screenWidth, _screenHeight));
-        Vector2 pointRecover = matScaleRecover.MultiplyPoint3x4(pointUV);
-        Debug.Log("Paint BrushWid:" + brushWid + " brushHei:" + brushHei + " angleOfLine:" + angleOfLine + " pointUV:" + pointUV +" pointRecover:" + pointRecover);
+        //Vector2 pointRecover = matScaleRecover.MultiplyPoint3x4(pointUV);
+        //Debug.Log("Paint BrushWid:" + brushWid + " brushHei:" + brushHei + " angleOfLine:" + angleOfLine + " pointUV:" + pointUV +" pointRecover:" + pointRecover);
 
         Vector2 brushDir = (point - _lastPoint).normalized;
         //bursh坐标的起点
@@ -239,17 +234,13 @@ public class PaintView : MonoBehaviour
         // 将四元数转换为矩阵
         Matrix4x4 matRot = Matrix4x4.Rotate(rotReverse);
         Vector3 rotatedPt = matRot.MultiplyPoint3x4(traslatedPt);
-        //Vector3 rotatedPt = matTranslate.Rotate(rotReverse);
 
-        //Vector3 brushScale = new Vector3(_brushRectangle.x / _screenWidth, _brushRectangle.y / _screenHeight, 1);
         Vector3 brushScale = new Vector3(1.0f / _brushRectangle.x, 1.0f /_brushRectangle.y, 1);
-        //Vector3 brushScale = new Vector3(1.0f / _screenWidth, 1.0f / _screenHeight, 1);
         Matrix4x4 matScale = Matrix4x4.Scale(brushScale);
         Vector3 scaledPt = matScale.MultiplyPoint3x4(rotatedPt);
 
         Matrix4x4 mat4 = matScale * matRot * matTranslate;
         Vector3 uvPt = mat4.MultiplyPoint3x4(point);
-        //_paintBrushMat.SetMatrix("_Matrix", mat4);
 
         Matrix4x4 matFromUV = matScale * matRot * matTranslate * matScaleRecover;
         Vector3 uvPoint = matFromUV.MultiplyPoint3x4(pointUV);
@@ -257,26 +248,7 @@ public class PaintView : MonoBehaviour
 
         //三个结果都是(0.5,0),说明是对的
         Debug.Log("Paint point:" + point + " originPt:" + originPt + " traslatedPt:" + traslatedPt + " rotatedPt:" + rotatedPt + " scaledPt:" + scaledPt + " uvPt" + uvPt + " uvPoint:" + uvPoint);
-
-
-
-        //Vector2 translate = Vector2.zero - originPt;
-        //Matrix4x4 m4 = Matrix4x4.identity;
-        //m4 = Matrix4x4.TRS(new Vector3(10, 10, 0), Quaternion.identity, Vector2.one);
-        //m_matrixBurshUV = Matrix4x4.TRS(new Vector3(10,10,0), Quaternion.identity, Vector2.one/2);//发现是首先scale，然后translate的
-        ////m_matrixBurshUV = Matrix4x4.TRS(translate, rot, brushScale);
-        //Vector2 testUV = m4.MultiplyPoint3x4(new Vector3(10,10,0));
-        //Vector3 newUV = m_matrixBurshUV.MultiplyPoint3x4(new Vector3(10, 10, 0));
-        //_paintBrushMat.SetVector("_BrushUV", new Vector2(newUV.x, newUV.y));
-        //Debug.Log("PaintView LerpPaint testUV:" + testUV + " newUV:" + newUV);
-        //Debug.Log("PaintView LerpPaint point:"+ point+ " originPt:"+ originPt + " translate:" + translate + " brushScale:" + brushScale +" newUV:" + newUV);
-
-        //Vector3 newPt = m4.MultiplyPoint3x4(point);
-        //oldGo.transform.position = new Vector3(point.x, point.y, 0);
-        //go.transform.position = newPt;
-        //go.transform.rotation = rot;
-        //go.transform.localScale = scale;
-
+        Paint(point);
 
         float dis = Vector2.Distance(point, _lastPoint);
         if (dis > _brushLerpSize)
@@ -299,163 +271,7 @@ public class PaintView : MonoBehaviour
         if (point.x < 0 || point.x > _screenWidth || point.y < 0 || point.y > _screenHeight)
             return;
 
-        Vector2 uv = new Vector2(point.x / (float)_screenWidth,
-            point.y / (float)_screenHeight);
-        //_paintBrushMat.SetVector("_UV", uv);
-
-        //float brushWid = _brushSize / _screenWidth;
-        //float brushHei = _brushSize / _screenHeight;
-        //_paintBrushMat.SetFloat("_BrushUVWidth", brushWid);
-        //_paintBrushMat.SetFloat("_BrushUVHeight", brushHei);
-        //Graphics.Blit(_renderTex, _renderTex, _paintBrushMat);
         Graphics.Blit(_renderTex, _canvasTex, _paintBrushMat);
-    }
-
-    void TestTRS()
-    {
-        Matrix4x4 m4 = Matrix4x4.identity;
-        //trs这是针对点的，要想让整个笔刷，即矩形整体旋转缩放，需要分别对矩形四个点都运算。
-        m4 = Matrix4x4.TRS(new Vector3(10, 10, 0), Quaternion.identity, Vector2.one);
-        m_matrixBurshUV = Matrix4x4.TRS(new Vector3(10, 10, 0), Quaternion.identity, Vector2.one / 2);//发现是首先scale，然后translate的
-        //m_matrixBurshUV = Matrix4x4.TRS(translate, rot, brushScale);
-        Vector2 testUV = m4.MultiplyPoint3x4(new Vector3(10, 10, 0));
-        Vector3 newUV = m_matrixBurshUV.MultiplyPoint3x4(new Vector3(10, 10, 0));
-        Debug.Log("PaintView LerpPaint testUV:" + testUV + " newUV:" + newUV);
-
-    }
-
-    /// <summary>
-    /// 重映射  默认  value 为1-100
-    /// </summary>
-    /// <param name="value"></param>
-    /// <param name="maxValue"></param>
-    /// <param name="minValue"></param>
-    /// <returns></returns>
-    private float Remap(float value, float startValue, float enValue)
-    {
-        float returnValue = (value - 1.0f) / (100.0f - 1.0f);
-        returnValue = (enValue - startValue) * returnValue + startValue;
-        return returnValue;
-    }
-
-    #endregion
-    private void TransformToWorldSpace(SubArray<Vector3> localSpaceVectors)
-    {
-        Matrix4x4 matrix = transform.localToWorldMatrix;
-        Vector3[] resultArray = localSpaceVectors.Array;
-        for (int i = 0; i < localSpaceVectors.Count; i++)
-            resultArray[i] = matrix.MultiplyPoint3x4(resultArray[i]);
-    }
-
-    private void TestMatrix()
-    {
-        Vector2 point = new Vector2(200,200);
-        Vector2 testPoint = new Vector2(270.7f,200);
-        Vector2 lastPoint = new Vector2(50,50);
-        _brushSize = 100;
-        _screenWidth = 400;
-        _screenHeight = 600;
-        float brushWid = _brushSize / _screenWidth;
-        float brushHei = _brushSize / _screenHeight;
-
-        float angleOfLine = Mathf.Atan2((point.y - lastPoint.y), (point.x - lastPoint.x)) * 180 / Mathf.PI;
-        Quaternion rot = Quaternion.Euler(0, 0, angleOfLine);
-        Quaternion rotReverse = Quaternion.Euler(0, 0, -angleOfLine);
-        //Matrix4x4 m = Matrix4x4.TRS(Vector3.zero, rot, Vector3.one);
-        // 将四元数转换为矩阵
-        Matrix4x4 matrix1 = Matrix4x4.Rotate(rotReverse);
-        // 应用矩阵变换到世界坐标
-        Vector2 leftPoint = new Vector2(_brushSize/2, _brushSize/2) - point;
-        
-        Vector3 newPosition = matrix1.MultiplyPoint(testPoint);
-        Vector3 newPosition1 = newPosition.Addition(leftPoint);
-        //Vector3 newPosition = Matrix4x4.Translate();
-        Debug.Log("Paint TestMatrix:" + brushWid + " brushHei:" + brushHei + " angleOfLine:" + angleOfLine + " leftPoint:" + leftPoint + " newPos:" + newPosition + " newPos1:" + newPosition1);
-
-
-        Matrix4x4 m4 = Matrix4x4.identity;
-        Vector3 scale = new Vector3(_brushSize / _screenWidth, _brushSize / _screenHeight, 1);
-        m4.SetTRS(new Vector3(-point.x, -point.y), rotReverse, scale);
-        _paintBrushMat.SetMatrix("_BrushRotation", m4);
-
-    }
-
-    /// <summary>
-    /// 平移矩阵对象
-    /// </summary>
-    /// <param name="pos"></param>
-    public Matrix4x4 MyTranslate(Vector3 pos)
-    {
-        //按照当前位置进行位移
-        //v = new Vector4(transform.position.x,transform.position.y,transform.position.z,1);
-
-        //按照原点进行位移
-        matrix = Matrix4x4.identity;
-
-        //X、Y、Z移动因子
-        matrix.m03 = pos.x;
-        matrix.m13 = pos.y;
-        matrix.m23 = pos.z;
-
-        return matrix;
-    }
-
-    /// <summary>
-    /// 缩放矩阵对象
-    /// </summary>
-    /// <param name="scale"></param>
-    public Matrix4x4 MyScale(Vector3 scale)
-    {
-        //设置当前对象大小
-        //v = new Vector4(transform.localScale.x, transform.localScale.y, transform.localScale.z, 1);
-
-        //按照原点进行位移
-        //matrix = Matrix4x4.identity;
-
-        //X、Y、Z缩放因子
-        matrix.m00 = scale.x;
-        matrix.m11 = scale.y;
-        matrix.m22 = scale.z;
-
-        return matrix;
-    }
-    public Matrix4x4 MyRotation(Axle axle, float angle)
-    {
-        //matrix = Matrix4x4.identity;
-
-        //对应 X、Y、Z的旋转
-        switch (axle)
-        {
-            case Axle.X:
-                matrix.m11 = Mathf.Cos(angle * Mathf.Deg2Rad);
-                matrix.m12 = -Mathf.Sin(angle * Mathf.Deg2Rad);
-                matrix.m21 = Mathf.Sin(angle * Mathf.Deg2Rad);
-                matrix.m22 = Mathf.Cos(angle * Mathf.Deg2Rad);
-                break;
-            case Axle.Y:
-                matrix.m00 = Mathf.Cos(angle * Mathf.Deg2Rad);
-                matrix.m02 = Mathf.Sin(angle * Mathf.Deg2Rad);
-                matrix.m20 = -Mathf.Sin(angle * Mathf.Deg2Rad);
-                matrix.m22 = Mathf.Cos(angle * Mathf.Deg2Rad);
-                break;
-            case Axle.Z:
-                matrix.m00 = Mathf.Cos(angle * Mathf.Deg2Rad);
-                matrix.m01 = -Mathf.Sin(angle * Mathf.Deg2Rad);
-                matrix.m10 = Mathf.Sin(angle * Mathf.Deg2Rad);
-                matrix.m11 = Mathf.Cos(angle * Mathf.Deg2Rad);
-                break;
-            default:
-                break;
-        }
-
-        float qw = Mathf.Sqrt(1f + matrix.m00 + matrix.m11 + matrix.m22) / 2;
-        float w = 4 * qw;
-        float qx = (matrix.m21 - matrix.m12) / w;
-        float qy = (matrix.m02 - matrix.m20) / w;
-        float qz = (matrix.m10 - matrix.m01) / w;
-
-        //transform.rotation = new Quaternion(qx, qy, qz, qw);
-        return matrix;
     }
 
     }
--- Assets/TestMatrixTRS.cs
+++ Assets/TestMatrixTRS.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using ToolBuddy.Pooling.Collections;
 using UnityEngine;
 
 public class TestMatrixTRS : MonoBehaviour
@@ -48,5 +49,73 @@ public class TestMatrixTRS : MonoBehaviour
 
         // Copy the transformed vertices back to the mesh.
         mf.mesh.vertices = newVerts;
+    }
+
+    void TestTRS()
+    {
+        Matrix4x4 m4 = Matrix4x4.identity;
+        //trs这是针对点的，要想让整个笔刷，即矩形整体旋转缩放，需要分别对矩形四个点都运算。
+        m4 = Matrix4x4.TRS(new Vector3(10, 10, 0), Quaternion.identity, Vector2.one);
+        Matrix4x4 m_matrixBurshUV = Matrix4x4.TRS(new Vector3(10, 10, 0), Quaternion.identity, Vector2.one / 2);//发现是首先scale，然后translate的
+        //m_matrixBurshUV = Matrix4x4.TRS(translate, rot, brushScale);
+        Vector2 testUV = m4.MultiplyPoint3x4(new Vector3(10, 10, 0));
+        Vector3 newUV = m_matrixBurshUV.MultiplyPoint3x4(new Vector3(10, 10, 0));
+        Debug.Log("PaintView LerpPaint testUV:" + testUV + " newUV:" + newUV);
+
+    }
+
+    /// <summary>
+    /// 重映射  默认  value 为1-100
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="maxValue"></param>
+    /// <param name="minValue"></param>
+    /// <returns></returns>
+    private float Remap(float value, float startValue, float enValue)
+    {
+        float returnValue = (value - 1.0f) / (100.0f - 1.0f);
+        returnValue = (enValue - startValue) * returnValue + startValue;
+        return returnValue;
+    }
+
+    private void TransformToWorldSpace(SubArray<Vector3> localSpaceVectors)
+    {
+        Matrix4x4 matrix = transform.localToWorldMatrix;
+        Vector3[] resultArray = localSpaceVectors.Array;
+        for (int i = 0; i < localSpaceVectors.Count; i++)
+            resultArray[i] = matrix.MultiplyPoint3x4(resultArray[i]);
+    }
+
+    private void TestMatrix()
+    {
+        //Vector2 point = new Vector2(200, 200);
+        //Vector2 testPoint = new Vector2(270.7f, 200);
+        //Vector2 lastPoint = new Vector2(50, 50);
+        //_brushSize = 100;
+        //_screenWidth = 400;
+        //_screenHeight = 600;
+        //float brushWid = _brushSize / _screenWidth;
+        //float brushHei = _brushSize / _screenHeight;
+
+        //float angleOfLine = Mathf.Atan2((point.y - lastPoint.y), (point.x - lastPoint.x)) * 180 / Mathf.PI;
+        //Quaternion rot = Quaternion.Euler(0, 0, angleOfLine);
+        //Quaternion rotReverse = Quaternion.Euler(0, 0, -angleOfLine);
+        ////Matrix4x4 m = Matrix4x4.TRS(Vector3.zero, rot, Vector3.one);
+        //// 将四元数转换为矩阵
+        //Matrix4x4 matrix1 = Matrix4x4.Rotate(rotReverse);
+        //// 应用矩阵变换到世界坐标
+        //Vector2 leftPoint = new Vector2(_brushSize / 2, _brushSize / 2) - point;
+
+        //Vector3 newPosition = matrix1.MultiplyPoint(testPoint);
+        //Vector3 newPosition1 = newPosition.Addition(leftPoint);
+        ////Vector3 newPosition = Matrix4x4.Translate();
+        //Debug.Log("Paint TestMatrix:" + brushWid + " brushHei:" + brushHei + " angleOfLine:" + angleOfLine + " leftPoint:" + leftPoint + " newPos:" + newPosition + " newPos1:" + newPosition1);
+
+
+        //Matrix4x4 m4 = Matrix4x4.identity;
+        //Vector3 scale = new Vector3(_brushSize / _screenWidth, _brushSize / _screenHeight, 1);
+        //m4.SetTRS(new Vector3(-point.x, -point.y), rotReverse, scale);
+        //_paintBrushMat.SetMatrix("_BrushRotation", m4);
+
     }
 }
